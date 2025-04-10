5bdf1a7b31d5f25eb00cefb6166a7ebcbf3abea9
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-10 09:49:00 +0200

    samuel oop __init__

--- sasa.py
+++ sasa.py
@@ -1,11 +1,31 @@
+# class CST:
+#     def IT(self,name,age,department):
+#         self.name=name
+#         self.age=age
+#         self.department=department
+# #declaration object called (samuel)
+# samuel=CST()
+# samuel.IT("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
+# print(samuel.name)
+# print(samuel.age)
+# print(samuel.department)
+   
+
+
+
+
+
 class CST:
-    def IT(self,name,age,department):
-        self.name=name
-        self.age=age
-        self.department=department
-#declaration object called (samuel)
-samuel=CST()
-samuel.IT("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
-print(samuel.name)
-print(samuel.age)
-print(samuel.department)
+    # Constructor to initialize the object
+    def __init__(self, name, age, department):
+        self.name = name
+        self.age = age
+        self.department = department
+
+# Create an object of the CST class
+samuel = CST("Bayishimire Samuel,gisubizo ernest,mr damoul", 224008071, "Information Technology")
+
+# Access and print the object's attributes
+print(samuel.name)         # Output: Bayishimire Samuel
+print(samuel.age)          # Output: 224008071
+print(samuel.department)    # Output: Information Technology
