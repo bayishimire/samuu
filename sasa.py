811fa94434cfb721104fb0da54d9d76c2eea89cf
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-10 09:07:40 +0200

    oop in python

---
+++ sasa.py
@@ -0,0 +1,11 @@
+class CST:
+    def IT(self,name,age,department):
+        self.name=name
+        self.age=age
+        self.department=department
+#declaration object called (samuel)
+samuel=CST()
+samuel.IT("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
+print(samuel.name)
+print(samuel.age)
+print(samuel.department)
