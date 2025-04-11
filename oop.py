9674dec6a5b03e92085b4c1550add8a05bc6dca1
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-11 12:59:05 +0400

    oop in herintance btn base as called parent and derived as called child class

---
+++ oop.py
@@ -0,0 +1,19 @@
+# parent class or base class called ("cst")
+class cst:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        # creat object called "IT"
+    def it(self):
+        print(f"{self.name}{self.age}")
+        #child class or derived class tha called ("ICT")
+    
+class ICT(cst):
+    # creat object called "comp"
+    def comp(self):
+        print(f"{self.name}{self.age}")
+#create object called oop 
+oop=ICT("BAYISHIMIRESAMUEL VS ERINEST GISUBIZO",20004) 
+oop.it()
+oop.comp()
+  
