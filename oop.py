3a58d1c692cfc9050d23984612394d7f3f6a59e6
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-11 14:16:20 +0400

    find len used in heritance of wads intering

--- oop.py
+++ oop.py
@@ -1,14 +1,22 @@
+class parentclass:
+    def __init__(self,name ,department):
+        self.name=name
+        self.department=department
+    def oop(self):
+        return f"{self.name}{self.department} is good name"
+class child(parentclass):
+    def derved(self):
+        return f"{self.name}{self.department} is good  name"
+object=child("samuel","ernest")
 
-class CST:
-      def __init__(self,name,age,department):
-          self.name=name
-          self.age=age
-          self.department=department
- # #declaration object called (samuel)
 
-samuel=CST("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
-print(samuel.name)
-print(samuel.age)
-print(samuel.department)
-    
- 
+print(object.derved())     
+print(object.oop())        
+
+x = len(object.derved())
+print(x )      # Nsamuelernest is goo name
+               #samuelernest is goodname
+               #24
+                 
+
+
