836900ddf07443619d8d911dbfe2f71f8a5de1c2
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-11 13:39:55 +0400

    __init__ oop

--- oop.py
+++ oop.py
@@ -1,12 +1,12 @@
 
 class CST:
-      def IT(self,name,age,department):
+      def __init__(self,name,age,department):
           self.name=name
           self.age=age
           self.department=department
  # #declaration object called (samuel)
-samuel=CST()
-samuel.IT("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
+
+samuel=CST("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
 print(samuel.name)
 print(samuel.age)
 print(samuel.department)
