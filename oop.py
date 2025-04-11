553026ef0c81dcd5b3c435d635b7c9545965a970
bayishimire <bayishimiresamuel760@gmail.com>
2025-04-11 13:34:43 +0400

    class introduction

--- oop.py
+++ oop.py
@@ -1,21 +1,14 @@
-# parent class or base class called ("cst")
-class cst:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-        # creat object called "IT"
-    def it(self):
-        print(f"{self.name}{self.age}")
-        #child class or derived class tha called ("ICT")
+
+class CST:
+      def IT(self,name,age,department):
+          self.name=name
+          self.age=age
+          self.department=department
+ # #declaration object called (samuel)
+samuel=CST()
+samuel.IT("bayishimire samuel,gisubizo ernest,mr damoul",2004, "informationtechnology")
+print(samuel.name)
+print(samuel.age)
+print(samuel.department)
     
-class ICT(cst):
-    # creat object called "comp"
-    def comp(self):
-        print(f"{self.name}{self.age}")
-#create object called oop 
-oop=ICT("BAYISHIMIRESAMUEL VS ERINEST GISUBIZO",20004) 
-oop.it()
-oop.comp()
-# out put BAYISHIMIRESAMUEL VS ERINEST GISUBIZO20004
-#BAYISHIMIRESAMUEL VS ERINEST GISUBIZO20004
-  
+ 
